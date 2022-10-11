787d30932f0f435572b75676b09857b477723b50
SamarthAnnigeri <Samarthgrand@gmail.com>
2022-10-11 10:14:45 -0700

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Samarth Annigeri on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
